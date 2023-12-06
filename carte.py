70e2d74dc37c69713d86f1d3c47150a9b949a372
mdjeghbala <mohamed.djeghbala@outlook.fr>
2023-12-06 15:39:47 +0100

    Ajout du dashboard avec une page web afin de pouvoir faire nos visualisations de donées sur la page

--- carte.py
+++ carte.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import folium
 
 pop_data = pd.read_csv('radars.csv', sep=',')
 print(pop_data)
--- main.py
+++ main.py
@@ -1,16 +1,55 @@
-import pandas as pd
-import dash as d
-import plotly as pl
+import plotly_express as px
 
-def main():
-    print_hi('PyCharm')
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print('Hi, {}'.format(name))  # Press Ctrl+F8 to toggle the breakpoint.
+import dash
+import dash_core_components as dcc
+from dash import html
 
-# Press the green button in the gutter to run the script.
+#
+# Data
+#
+
+year = 2002
+
+gapminder = px.data.gapminder() # (1)
+years = gapminder["year"].unique()
+data = { year:gapminder.query("year == @year") for year in years} # (2)
+
+#
+# Main
+#
+
 if __name__ == '__main__':
-    main()
 
-    #f = pd.read_csv('radars.csv')
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    app = dash.Dash(__name__) # (3)
+
+    fig = px.scatter(data[year], x="gdpPercap", y="lifeExp",
+                        color="continent",
+                        size="pop",
+                        hover_name="country") # (4)
+
+
+    app.layout = html.Div(children=[
+
+                            html.H1(children=f'Life expectancy vs GDP per capita ({year})',
+                                        style={'textAlign': 'center', 'color': '#7FDBFF'}), # (5)
+
+                            dcc.Graph(
+                                id='graph1',
+                                figure=fig
+                            ), # (6)
+
+                            html.Div(children=f'''
+                                The graph above shows relationship between life expectancy and
+                                GDP per capita for year {year}. Each continent data has its own
+                                colour and symbol size is proportionnal to country population.
+                                Mouse over for details.
+                            '''), # (7)
+
+    ]
+    )
+
+    #
+    # RUN APP
+    #
+
+    app.run_server(debug=True) # (8)
