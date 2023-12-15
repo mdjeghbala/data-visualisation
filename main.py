20fde5ec5cadde2b2b454720e00e3eaa2ba0f129
mdjeghbala <mohamed.djeghbala@outlook.fr>
2023-12-15 14:25:14 +0100

    Suppresion de code inutile

--- main.py
+++ main.py
@@ -1,14 +1,10 @@
 from dash import Dash, dcc, html
-from map import create_radars_numbers_map
-
 
 if __name__ == '__main__':
 
     # Création de l'application du dashboard
     app = Dash(__name__)
 
-    # Creation de la carte radar folium
-    card = create_radars_numbers_map('radars.csv')
     app.layout = html.Div([
         dcc.Markdown("# Le nombre de radars par département"),
         # Intégration de la carte
